75216e2a5c283358cbdb9a1d8b8f0b2cba55d317
Andrew Disley <andrew@simplified.co.uk>
2020-08-01 18:46:06 +0100

    Adds _redirects

--- .eleventy.js
+++ .eleventy.js
@@ -78,6 +78,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
+  eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("favicon.ico");
